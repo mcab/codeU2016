079941d7f1a58647e9b07b9eb28c76493236b106
Mark Cabanero <mcab@users.noreply.github.com>
2016-08-09 15:47:36 -0700

    Fix merge conflicts.

--- src/main/java/codeU/EntryPoint.java
+++ src/main/java/codeU/EntryPoint.java
@@ -12,14 +12,9 @@ public class EntryPoint {
     @POST
     @Path("post")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-<<<<<<< HEAD
-    @Produces(MediaType.TEXT_HTML)
-    public String postMethod(@FormParam("search") String search) throws IOException,InterruptedException,ExecutionException {
-=======
     @Produces("text/html")
-    public String postMethod(@FormParam("search") String search) throws IOException {
+    public String postMethod(@FormParam("search") String search) throws IOException, InterruptedException, ExecutionException  {
         final long startTime = System.currentTimeMillis();
->>>>>>> 43c29606a79e8608706821b86b5d8a3c7fad9f37
         WikiMain w = new WikiMain();
         StringBuilder sb = new StringBuilder();
         List<Map.Entry<String, Double>> entries = w.searcher(new String[]{search});
@@ -31,10 +26,6 @@ public class EntryPoint {
         for (Map.Entry<String, Double> url : entries) {
             sb.append("\t\t\t\t<h4><li><a href=" + url.getKey() + ">" + cleanURL(url.getKey().substring(30)) + "</a>: " + url.getValue() + "</li></h4>\n");
         }
-<<<<<<< HEAD
-        //window.open("data:text/html;charset=utf-8,"+html, "", "_blank")
-        return "<h3>Results for the query " + search + "<br>" + sb;
-=======
         final long endTime = System.currentTimeMillis();
         return "<html>\n<head>" +
                 "\n\t<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">" +
@@ -45,7 +36,6 @@ public class EntryPoint {
                 "\n\n<h6>This page took " + (endTime - startTime) + " ms to render." +
                 "\n\t</div>" +
                 "\n</body>\n</html>";
->>>>>>> 43c29606a79e8608706821b86b5d8a3c7fad9f37
     }
     private String cleanURL(String url){
         String clean = url;
