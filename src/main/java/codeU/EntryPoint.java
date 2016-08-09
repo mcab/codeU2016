43c29606a79e8608706821b86b5d8a3c7fad9f37
Mark Cabanero <mcab@users.noreply.github.com>
2016-08-09 15:32:40 -0700

    Added timing, removed unnecessary variable.

--- src/main/java/codeU/EntryPoint.java
+++ src/main/java/codeU/EntryPoint.java
@@ -13,6 +13,7 @@ public class EntryPoint {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces("text/html")
     public String postMethod(@FormParam("search") String search) throws IOException {
+        final long startTime = System.currentTimeMillis();
         WikiMain w = new WikiMain();
         StringBuilder sb = new StringBuilder();
         List<Map.Entry<String, Double>> entries = w.searcher(new String[]{search});
@@ -21,17 +22,17 @@ public class EntryPoint {
             return "There are no search results for " + search;
         }
 
-        int urlNumber = 1;
         for (Map.Entry<String, Double> url : entries) {
             sb.append("\t\t\t\t<h4><li><a href=" + url.getKey() + ">" + cleanURL(url.getKey().substring(30)) + "</a>: " + url.getValue() + "</li></h4>\n");
-            urlNumber += 1;
         }
+        final long endTime = System.currentTimeMillis();
         return "<html>\n<head>" +
                 "\n\t<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">" +
                 "\n</head>\n<body>" +
                 "\n\t<div class=\"container\">" +
                 "\n\t\t<h2>Results for the word " + search + "</h2>" +
                 "\n\t\t\t<ol>\n" + sb + "\t\t\t</ol>" +
+                "\n\n<h6>This page took " + (endTime - startTime) + " ms to render." +
                 "\n\t</div>" +
                 "\n</body>\n</html>";
     }
