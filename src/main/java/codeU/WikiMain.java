56b331255a33c41c1ca31389be3ad1ef364eb59a
Mark Cabanero <mcab@users.noreply.github.com>
2016-08-09 14:35:23 -0700

    Refactored for readability.

--- src/main/java/codeU/WikiMain.java
+++ src/main/java/codeU/WikiMain.java
@@ -15,74 +15,43 @@ public class WikiMain {
 
     public List<Map.Entry<String, Double>> searcher(String[] search) throws IOException {
         Indexer index = new Indexer();
-        boolean searched = false;
-
-        //start prompt
-        System.out.println("WIKIPEDIA SEARCH");
-        System.out.println("Searching for: " + search[0]);
-        while(!searched){
-            String query = search[0];
-            List<String> words = Arrays.asList(query.split(" "));
-            String sourceUrl = "https://en.wikipedia.org/w/index.php?title=Special:Search&limit=30&profile=advanced&profile=advanced&fulltext=Search&search=";
-            if (words.size() > 2){
-                String term1 = words.get(0);
-                String term2 = words.get(2);
-                sourceUrl += term1;
-                crawl(sourceUrl, words, query, index);
-                if(words.get(1).equals("&&")){
-                    searched = true;
-                    WikiSearch ws1 = search(term1,index);
-                    WikiSearch ws2 = search(term2,index);
-                    if(ws1 == null || ws2 == null){
-                        return null;
-                    }else {
-                        WikiSearch ws = ws1.and(ws2);
-                        return ws.sort();
-                    }
-                }else if(words.get(1).equals("||")){
-                    searched = true;
-                    WikiSearch ws1 = search(term1,index);
-                    WikiSearch ws2 = search(term2,index);
-                    if(ws1 == null || ws2 == null){
-                        return null;
-                    }else {
-                        WikiSearch ws = ws1.or(ws2);
-                        return ws.sort();
-                    }
-                }else if(words.get(1).equals("-")){
-                    searched = true;
-                    WikiSearch ws1 = search(term1,index);
-                    WikiSearch ws2 = search(term2,index);
-                    if(ws1 == null || ws2 == null){
-                        return null;
-                    }else {
-                        WikiSearch ws = ws1.minus(ws2);
-                        return ws.sort();
-                    }
-                }else{
-                    System.out.println("Try Again!");
-                    continue;
+        System.out.println("WIKIPEDIA SEARCH, searching for: " + search[0]);
+        String query = search[0];
+        List<String> words = Arrays.asList(query.split(" "));
+        String sourceUrl = "https://en.wikipedia.org/w/index.php?title=Special:Search&limit=30&profile=advanced&profile=advanced&fulltext=Search&search=";
+        if (words.size() > 2) {
+            String term1 = words.get(0);
+            String term2 = words.get(2);
+            sourceUrl += term1;
+            crawl(sourceUrl, words, query, index);
+            WikiSearch ws1 = search(term1, index);
+            WikiSearch ws2 = search(term2, index);
+            if (ws1 == null || ws2 == null) {
+                return null;
+            } else {
+                if (words.get(1).equals("&&")) {
+                    WikiSearch ws = ws1.and(ws2);
+                    return ws.sort();
+                } else if (words.get(1).equals("||")) {
+                    WikiSearch ws = ws1.or(ws2);
+                    return ws.sort();
+                } else if (words.get(1).equals("-")) {
+                    WikiSearch ws = ws1.minus(ws2);
+                    return ws.sort();
                 }
-            }else if(words.size() == 1){
-                sourceUrl+=words.get(0);
-                searched = true;
+            }
+        } else if (words.size() == 1) {
+                sourceUrl += words.get(0);
                 crawl(sourceUrl, words, query, index);
                 WikiSearch ws = search(words.get(0), index);
-                if(ws == null){
+                if (ws == null) {
                     return null;
                 }
-                System.out.println("-------------------RESULTS-------------------");
                 return ws.sort();
-            }else{
-                System.out.println("Try Again!");
-                continue;
-            }
-
         }
         return null;
     }
-
-
+    
     private static String query(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a query or type x to quit: ");
